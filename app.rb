3602dfeff63d8945aa9338dfd51fb9003b54fe12
Adrienne Yao <adrienne.yao@icloud.com>
2019-10-29 15:21:39 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :session_secret, 'secretpassword'
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    session[:item] = params[:item]
+    @session = session
+    erb :checkout
+  end
+
 end
